81baf99484596a9160773b26579bac5f5418f161
Lee Sang Hun <zasfe@users.noreply.github.com>
2015-08-04 11:57:03 +0900

    Update ipv6_disable_CentOS6.sh

--- tip/ipv6_disable_CentOS6.sh
+++ tip/ipv6_disable_CentOS6.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+
+#
+# Disable IPv6
+#
+# http://www.itzgeek.com/how-tos/mini-howtos/disable-ipv6-on-centos-6-rhel-6.html
+# https://mbrownnyc.wordpress.com/2012/09/18/completely-disable-ipv6-in-centos6/
+
 echo "install ipv6 /bin/true" > /etc/modprobe.d/ipv6_disabled.conf
 
 echo "NETWORKING_IPV6=no" >> /etc/sysconfig/network
