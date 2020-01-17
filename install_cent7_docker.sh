f357271c1c49e4b2c8882b4287c6e444643661c3
Lee Sang Hun <zasfe@users.noreply.github.com>
2020-01-17 12:34:28 +0900

    Update install_cent7_docker.sh

--- install_cent7_docker.sh
+++ install_cent7_docker.sh
@@ -8,6 +8,12 @@ source /etc/profile.d/history.sh
 # docker install
 curl -fsSL https://get.docker.com/ | sudo sh
 
+systemctl start docker.service
+systemctl enable docker.service
+systemctl status docker.service
+docker version
+
+
 # docker pull apache/zeppelin
 docker pull apache/zeppelin:0.8.2
 docker pull apache/zeppelin:0.7.2
@@ -15,6 +21,5 @@ docker pull apache/zeppelin:0.7.2
 mkdir -p logs
 mkdir -p notebook
 
-docker run -p 8080:8080 -rm -name zeppelin apache/zeppelin:0.7.2
-
-docker run -p 8080:8080 -rm -v $PWD/logs:/logs -v $PWD/notebook:/notebook -e ZEPPELIN_LOG_DIR=’/logs’ -e ZEPPELIN_NOTEBOOK_DIR=’/notebook’ -name zeppelin apache/zeppelin:0.7.2
+# docker run -p 8080:8080 -rm -name zeppelin apache/zeppelin:0.7.2
+# docker run -p 8080:8080 -rm -v $PWD/logs:/logs -v $PWD/notebook:/notebook -e ZEPPELIN_LOG_DIR=’/logs’ -e ZEPPELIN_NOTEBOOK_DIR=’/notebook’ -name zeppelin apache/zeppelin:0.7.2
